8785b8748e0cd274155a1bdc9f43e0467f5c9e49
ArtemRysich <110498892+ArtemRysich@users.noreply.github.com>
2023-10-04 14:16:50 +0300

    Lesson_15

---
+++ Lesson_15/src/helpers.js
@@ -0,0 +1,29 @@
+// const valA = 55;
+
+// function add(a, b) {
+//   return a + b + valA;
+// }
+// function sum(a, b) {
+//   return a * b;
+// }
+
+// const value = 5;
+// export { add, sum, value };
+
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//   }
+// }
+
+// export default User;
+
+
+
+
+
+
+
+export default 20;
+export default 10;
---
+++ Lesson_15/src/index.js
@@ -0,0 +1,29 @@
+// Шаблонізатор
+// https://handlebarsjs.com/
+
+// Інструкція роботи з гіт
+// https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories#changing-a-remote-repositorys-url
+// https://youtu.be/wFY5HVuQBgw
+// https://w3schoolsua.github.io/git/index.html
+
+// транскомпілятор JavaScript
+// https://babeljs.io/
+
+
+// import  {add} from './helpers'
+// import qwerty from './helpers'
+
+// console.log(add(5, 25));
+// import * as common from './helpers';
+// console.log(common.add(5,5));
+// console.log(common.value);
+// console.log(qwerty);
+// import {value as helperValue } from './helpers'
+
+
+// const value = 20;
+
+// console.log(value);
+// console.log(helperValue)
+
+
